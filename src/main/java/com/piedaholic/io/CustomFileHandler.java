6df4e58f5201fe609287ea93b42fd34b65ce05c6
Harsh Prakash Singh <harsh113300@nitp.ac.in>
2021-05-07 03:22:25 +0530

    Imported java.nio.Charset :: Default charsets provided by Java

--- src/main/java/com/piedaholic/io/CustomFileHandler.java
+++ src/main/java/com/piedaholic/io/CustomFileHandler.java
@@ -1,27 +1,31 @@
 package com.piedaholic.io;
 
 import java.io.File;
+import java.nio.charset.Charset;
 
 public class CustomFileHandler {
 
     public final static String fileSeparator = System.getProperty("file.separator");
     // public final static String fileSeparator = java.io.File.separator;
-    
-    public boolean create(String path) throws SecurityException{
-     File f = new File(path);
-     return f.mkdirs();
-    }
+    Charset usAsciiCharset = Charset.forName("US-ASCII");
+    Charset utf8Charset = Charset.forName("UTF-8");
+    Charset utf16Charset = Charset.forName("UTF-16");
 
-    public boolean create(File f) throws SecurityException{
+    public boolean create(String path) throws SecurityException {
+        File f = new File(path);
         return f.mkdirs();
     }
 
-    public boolean delete(String path) throws SecurityException{
+    public boolean create(File f) throws SecurityException {
+        return f.mkdirs();
+    }
+
+    public boolean delete(String path) throws SecurityException {
         File f = new File(path);
         return f.delete();
     }
 
-    public boolean delete(File f) throws SecurityException{
+    public boolean delete(File f) throws SecurityException {
         return f.delete();
     }
 }
