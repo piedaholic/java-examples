4fafbd0f35dcf2ed4d2ad682ed44a1f9a5a8b4fc
Harsh Prakash Singh <harsh113300@nitp.ac.in>
2022-01-08 22:22:43 +0530

    Abstract Class Implementation

---
+++ src/main/java/com/piedaholic/class_design/abstraction/AbstractA.java
@@ -0,0 +1,14 @@
+package com.piedaholic.class_design.abstraction;
+
+public abstract class AbstractA {
+
+	public abstract void peek();
+
+	public static void walk() {
+		System.out.println("walk");
+	}
+
+	public void mate() {
+		System.out.println("mate");
+	}
+}
